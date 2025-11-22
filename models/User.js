169d2bfa535e7f410cb0136abf902bd36d819ec9
Cartlal <147944223+Cartlal@users.noreply.github.com>
2025-11-22 10:47:21 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,5 @@
-import mongoose from "mongoose";
-
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
-});
-
-export default mongoose.model("User", UserSchema);
+  name: String,
+  email: { type: String, unique: true },
+  password: String
+}, { timestamps: true }); // <<< important
